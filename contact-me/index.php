b4201e938771e717ff3ee47211b886599dea0fb9
robertmaxwilliams <robertmaxwilliams@gmail.com>
2016-07-08 17:41:24 -0400

    uploaded the whole everything

---
+++ contact-me/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<body>
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/header.html";
+   include_once($path);
+?>
+<script>
+document.getElementById("contact-me").style.color = "black";
+</script>
+
+<div class="body">
+<h1>Robert Max Williams</h1>
+<h2><i>"I know what I'm doing"</i></h2>
+<p>Robert Max Williams</p>
+<p><a href="mailto:robertmaxwilliams@gmail.com?Subject=Hello%Max" target="_top">robertmaxwilliams@gmail.com</a></p>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<body>
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/header.html";
+   include_once($path);
+?>
+<script>
+document.getElementById("home").style.color = "black";
+</script>
+
+<div class="body">
+<h1>Welcome to extraboat</h1>
+<h2><i>"The only site for your extraboat needs"</i></h2>
+<p>Actually it's just a developer site. If you want something built like this, don't hesitate to contact me.</p>
+
+</div>
+</body>
+</html>
